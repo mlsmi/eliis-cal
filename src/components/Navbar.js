77e14379d98cef5e449fe3eb895293721fbd7391
mlsmi <11808906+mlsmi@users.noreply.github.com>
2023-11-29 01:30:40 +0200

    refactor navbar icons from hardcoded to using data + design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,14 @@ function getInitials(firstname, lastname) {
   return firstname[0] + lastname[0];
 }
 
+const navIcons = [
+  { icon: mdiBell, notification: notificationsData.bell },
+  { icon: mdiBullhorn },
+  { icon: mdiMessage, notification: notificationsData.message },
+  { icon: mdiCalendar, notification: notificationsData.calendar },
+  { icon: mdiCloud },
+];
+
 function Navbar() {
   return (
     <nav className="flex items-center justify-between p-4 navbar border-b border-gray h-16">
@@ -16,33 +24,19 @@ function Navbar() {
         <img src={logo} alt="logo" className="w-24 h-8" />
       </div>
       <div className="hidden md:flex items-center space-x-2">
-        <div className="flex space-x-2">
-          <div className="bg-orange-500/60 rounded-full p-2 relative">
-            <Icon path={mdiBell} size={1} color="white" />
-            <div className="greenbg absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-              <span>{notificationsData.bell}</span>
+        <div className="flex space-x-1">
+          {navIcons.map((navIcon, index) => (
+            <div key={index} className="bg-orange-500/60 rounded-full p-2 relative">
+              <Icon path={navIcon.icon} size={0.8} color="white" />
+              {navIcon.notification && (
+                <div className="greenbg absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                  <span>{navIcon.notification}</span>
+                </div>
+              )}
             </div>
-          </div>
-          <div className="bg-orange-500/60 rounded-full p-2">
-            <Icon path={mdiBullhorn} size={1} color="white" />
-          </div>
-          <div className="bg-orange-500/60 rounded-full p-2 relative">
-            <Icon path={mdiMessage} size={1} color="white" />
-            <div className="greenbg absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-              <span>{notificationsData.message}</span>
-            </div>
-          </div>
-          <div className="bg-orange-500/60 rounded-full p-2 relative">
-            <Icon path={mdiCalendar} size={1} color="white" />
-            <div className="greenbg absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-              <span>{notificationsData.calendar}</span>
-            </div>
-          </div>
-          <div className="bg-orange-500/60 rounded-full p-2">
-            <Icon path={mdiCloud} size={1} color="white" />
-          </div>
+          ))}
         </div>
-        <div className="flex flex-col items-start relative vertical-border pl-6">
+        <div className="flex flex-col items-start relative vertical-border pl-4">
           <span className="nav-user text-sm font-bold">{userData.firstname} {userData.lastname}</span>
           <span className="nav-kindergarten text-sm">{userData.kindergarten}</span>
         </div>
