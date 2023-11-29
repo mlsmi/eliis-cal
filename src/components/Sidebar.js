7c3adf0d60637d5964efe8bd9f61e583acc2fcb9
mlsmi <11808906+mlsmi@users.noreply.github.com>
2023-11-29 02:45:30 +0200

    sidebar menuitem children bg styling and menuitem link fixes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,27 +15,42 @@ function Sidebar() {
     return (
       <div className="hidden md:block md:w-72 h-screen px-4 border-r border-gray">
         {menuItems.map((item, index) => (
-          <div key={item.title} className="my-2">
-            <div className="menu-item-gradient p-1 border border-gray rounded-md" onClick={() => handleClick(index)}>
-              <div className="flex justify-between items-center">
-                <div className="flex items-center">
-                  <div className="bg-orange-500/60 rounded-md p-2 inline-block mr-2">
-                    <Icon path={item.icon} size={0.6} color="white" className="transform scale-125" />
+          <div key={item.title} className="my-2 bg-gray-500/10 rounded-md">
+            {!item.children ? (
+              <Link to={`/${item.slug}`}>
+                <div className="menu-item-gradient p-1 border border-gray rounded-md">
+                  <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                      <div className="bg-orange-500/60 rounded-md p-2 inline-block mr-2">
+                        <Icon path={item.icon} size={0.6} color="white" className="transform scale-125" />
+                      </div>
+                      <span className={`text-sm font-semibold ${location.pathname === `/${item.slug}` ? 'text-orange-500' : ''}`}>{item.title}</span>
+                    </div>
                   </div>
-                  <span className={`text-sm font-semibold ${location.pathname === `/${item.slug}` ? 'text-orange-500' : ''}`}>{item.title}</span>
                 </div>
-                {item.children && (
-                  <Icon path={open[index] ? mdiChevronDown : mdiChevronRight} size={1} color='black' />
-                )}
-              </div>
-            </div>
-            {item.children && open[index] && (
-              <div className="bg-gray-500/10 rounded-md py-1">
-                {item.children.map((child) => (
-                  <div key={child.title} className="py-1 ml-3">
-                    <Link to={`/${item.slug}/${child.slug}`} className={`text-sm ${location.pathname === `/${item.slug}/${child.slug}` ? 'text-orange-500' : ''}`}>{child.title}</Link>
+              </Link>
+            ) : (
+              <div>
+                <div className="menu-item-gradient p-1 border border-gray rounded-md" onClick={() => handleClick(index)}>
+                  <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                      <div className="bg-orange-500/60 rounded-md p-2 inline-block mr-2">
+                        <Icon path={item.icon} size={0.6} color="white" className="transform scale-125" />
+                      </div>
+                      <span className={`text-sm font-semibold ${location.pathname === `/${item.slug}` ? 'text-orange-500' : ''}`}>{item.title}</span>
+                    </div>
+                    <Icon path={open[index] ? mdiChevronDown : mdiChevronRight} size={1} color='black' />
                   </div>
-                ))}
+                </div>
+                {item.children && open[index] && (
+                  <div className="py-1">
+                    {item.children.map((child) => (
+                      <div key={child.title} className="py-1 ml-3">
+                        <Link to={`/${item.slug}/${child.slug}`} className={`text-sm ${location.pathname === `/${item.slug}/${child.slug}` ? 'text-orange-500' : ''}`}>{child.title}</Link>
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
             )}
           </div>
