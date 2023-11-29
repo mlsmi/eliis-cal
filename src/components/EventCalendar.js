c1fb549e2f09ddbad0b88e90c60e335f18402b9f
mlsmi <11808906+mlsmi@users.noreply.github.com>
2023-11-29 02:08:12 +0200

    eventTypes data and types sidebar for calendar component

--- src/components/EventCalendar.js
+++ src/components/EventCalendar.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { eventTypes } from '../eventTypes';
+import Icon from '@mdi/react';
+import { mdiCheck } from '@mdi/js';
 
 function EventCalendar() {
   return (
     <div className="flex-grow bg-white p-4">
-      <h1 className="text-3xl mb-4">Event Calendar</h1>
+      <h1 className="text-2xl mb-4">Event Calendar</h1>
+      <div className="hidden md:flex">
+        <div className="md:w-52 h-screen border-r border-gray">
+          <h3 className="text-sm font-semibold">TYPES</h3>
+          <ul>
+            {eventTypes.map((type, index) => (
+              <li key={index} className="flex items-center text-sm py-1">
+                <span className="inline-block w-5 h-5 rounded-full mr-2 flex items-center justify-center" 
+                      style={{
+                        backgroundColor: type.title === 'Public holidays' ? 'white' : type.color,
+                        border: type.title === 'Public holidays' ? `1px solid ${type.color}` : 'none'
+                      }}>
+                  <Icon path={mdiCheck} size={0.5} color={type.title === 'Public holidays' ? type.color : 'white'} />
+                </span>
+                {type.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex-grow ml-4">
+          {/* Main Calendar content goes here */}
+          <p>Calendar</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/eventTypes.js
@@ -0,0 +1,38 @@
+export const eventTypes = [
+    {
+      title: 'Education project',
+      color: '#F69D80',
+    },
+    {
+      title: 'Theatre/Concert',
+      color: '#A5C365',
+    },
+    {
+      title: 'Meeting',
+      color: '#DF83B1',
+    },
+    {
+      title: 'Training',
+      color: '#EB5E5D',
+    },
+    {
+      title: 'Joint event',
+      color: '#CC7979',
+    },
+    {
+      title: 'Class event',
+      color: '#EFAD4E',
+    },
+    {
+      title: 'Learning activity',
+      color: '#72CAC2',
+    },
+    {
+      title: 'Other event',
+      color: '#5BC1DE',
+    },
+    {
+      title: 'Public holidays',
+      color: '#EB5E5D',
+    },
+  ];
